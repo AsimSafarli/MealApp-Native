21a7937439c8f3640cc56b58cc06d2ee37c73e55
AsimSafarli <asimseferli014@gmail.com>
2023-09-10 13:25:06 +0400

    add navigatior

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './Screen/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
     <>
-    <StatusBar style='light' />
-    <CategoriesScreen />
+    <StatusBar style='black' />
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen  name='Meals Categories' component={CategoriesScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
   </>
   );
 }

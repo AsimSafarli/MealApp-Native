6932319587ecb0a271206384063edfb9c944ced7
AsimSafarli <asimseferli014@gmail.com>
2023-09-11 07:08:42 +0400

    modified meal detail page

--- Components/MealItem.js
+++ Components/MealItem.js
@@ -28,11 +28,7 @@ import {
               <Image source={{ uri: imageUrl }} style={styles.image} />
               <Text style={styles.title}>{title}</Text>
             </View>
-            <View style={styles.details}>
-              <Text style={styles.detailItem}>{duration}m</Text>
-              <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-              <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-            </View>
+          
           </View>
         </Pressable>
       </View>
--- Screen/MealDetailPage.js
+++ Screen/MealDetailPage.js
@@ -1,13 +1,39 @@
 import React from 'react'
-import { Text } from 'react-native'
-
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { MEALS } from '../Data/Data'
+import MealDetails from '../Components/MealDetails'
+import Subtitle from '../Components/Subtitle'
+import List from '../Components/List'
 function MealDetailPage({ route }) {
     const mealId = route.params.mealId
-    const mealTitle = route.params.mealTitle
-
+    const selectMeal = MEALS.find((meal)=> meal.id ===  mealId)
+   
       return (
-    <Text> MealDetailPage meal - {mealId} </Text>
+    <ScrollView>
+      <Image source={{uri:selectMeal.imageUrl}} style={styles.image}/>
+      <Text style={styles.title}>{selectMeal.title}</Text>
+      <MealDetails duration={selectMeal.duration} complexity={selectMeal.complexity}
+        affordability={selectMeal.affordability}/>
+        <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectMeal.steps} />
+        </View>
+      </View>
+     </ScrollView>
   )
 }
-
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200,
+      },
+      title:{
+        fontWeight:'bold',
+        fontSize:24,
+        textAlign:'center'
+      }
+})
 export default MealDetailPage
